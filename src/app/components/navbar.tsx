4e543175baad3c953241dbd64b0d25f559a70385
Thitipat Na Nakorn <thitipat.n@arise.tech>
2023-11-09 16:26:07 +0700

    chore add about route

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,20 +1,24 @@
-
-"use client"
+"use client";
 import React from "react";
 import { useAuth } from "../context/authContext";
-
+import Link from "next/link";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
   const { user, logout } = useAuth();
-  if(!!user == false){
-    return <></>
+  if (!!user == false) {
+    return <></>;
   }
   return (
     <>
       <div className="flex flex-row justify-between p-3 bg-red-300">
-        <p>Test Login</p>
+        <div className="flex gap-6">
+        <Link href={"/"}>Test Login</Link>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/login"}>Login</Link>
+        </div>
         {user && <p>{user}</p>}
         {!!user && (
           <button
